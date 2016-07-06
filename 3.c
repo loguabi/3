9ffc21de3d2c6bda13a760fdf69caaf4ea98f54c
logitha <loga848.456@gmail.com>
2016-07-06 12:12:06 +0530

    Create 3.c

---
+++ 3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int x,y,x1,y1;
+for(x=0;x<15;x++)
+x1+=x;
+printf("1 to 15 %d x1=%d\n",x1);
+for(y=0;y<45;y++);
+if(y%2!=0)
+y1+=y;
+printf("15 to 45  y1=%d\n",y1);
+return 0;
+}
